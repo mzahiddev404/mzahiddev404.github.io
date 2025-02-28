3ba365bd66f0552aeb02ed9ffb12fb0c7edd7345
mzahiddev404 <mzahid.dev404@gmail.com>
2025-02-28 15:44:59 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
